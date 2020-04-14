5475e983daaad2f72bebd902629eb0f49f5279a9
BrandiPSmith <brandis2@illinois.edu>
2020-04-14 02:21:40 -0500

    Update to pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,32 +1,51 @@
-#connect to database
 #!/usr/bin/python
 
 import sqlite3
 import os
 import pandas as pd
+import re
 
 #get working directory
 print(os.getcwd())
 
-#connect to database - database large so have to download to local computer
-DB = sqlite3.connect('/Users/brandismith/Documents/Tagger Data/all_predictions_1987_2019')
-print("Opened database successfully");
+#path to database
+pathDB='/Users/brandismith/Documents/Tagger Data/all_predictions_1987_2019'
 
-#make variable for old taggers -- may take some time; very large and have to download to local computer
-rctTags=pd.read_table("/Users/brandismith/Documents/Tagger Data/all.csv")
+#connect to database
+DB = sqlite3.connect(pathDB)
+
+#path to old taggers
+pathOld='/Users/brandismith/Documents/Tagger Data/all.csv'
+
+#make variable for old taggers
+rctTags=pd.read_table(pathOld)
 
 #select RCT Tagger Table
 rctDB = DB.execute("SELECT * FROM RANDOMIZED_CONTROLLED_TRIAL")
+#close connection to database
 DB.close()
 
-#read starting PubMed IDs
+#read starting PubMed IDs from search
 startIds=pd.read_csv('OCR_RCT_START_IDS.txt', header=None)
+#name columns year and pubmed ID
 startIds.rename(columns={0: 'Year', 1: 'PMID'}, inplace=True)
-#start.ids<-str_replace_all(start.ids$PubMed.ID, "[.].*","")
+#remove expressions from pubmed IDs
+# REF: https://stackoverflow.com/questions/13682044/remove-unwanted-parts-from-strings-in-a-column
+startIds['PMID'] = startIds['PMID'].str.split(r'\D').str.get(1)
+print(startIds)
 
-#read retrieved abstracts
+
+#read retrieved abstract PubMed IDs
 absRet=pd.read_csv('OCR_RCT_RET_ABS_IDS.txt', header=None)
+#name columns year and pubmed ID
 absRet.rename(columns={0: 'Year', 1: 'PMID'}, inplace=True)
-#abs.ret<-str_replace_all(abs.ret$PubMed.ID, "[.].*","")
+#remove expressions from pubmed IDs
+absRet['PMID'] = absRet['PMID'].str.split(r'\D').str.get(1)
+print(absRet)
+
+#match ids to database and csv files
+#REF: https://datatofish.com/sql-to-pandas-dataframe/
 
 
+
+#print results
