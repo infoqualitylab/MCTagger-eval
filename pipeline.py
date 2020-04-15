d3ad0a7baa5895d8f04040e4f4cdb024b587e090
BrandiPSmith <brandis2@illinois.edu>
2020-04-14 21:03:19 -0500

    Update to pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -25,6 +25,8 @@ rctTags=pd.read_table(pathOld)
 #REF: https://datacarpentry.org/python-ecology-lesson/09-working-with-sql/index.html
 rctDf = pd.read_sql_query("SELECT * FROM RANDOMIZED_CONTROLLED_TRIAL", DB)
 
+#what if I want to retrieve more than one table ? Or all tables?
+
 #close connection to database
 DB.close()
 
@@ -54,10 +56,10 @@ len(startIds.index)#6827 rows
 
 
 #read retrieved abstract PubMed IDs
-absRet=pd.read_csv('OCR_RCT_RET_ABS_IDS.txt', header=None)
+absRet=pd.read_csv('updated_abst_list.txt', header=None)
 #name columns year and pubmed ID
 absRet.rename(columns={0: 'Year', 1: 'PMID'}, inplace=True)
-#remove expressions from pubmed IDs
+#remove expressions from pubmed IDs; some IDs may not be PMIDs
 absRet['PMID'] = absRet['PMID'].str.split(r'\D').str.get(1)
 print(absRet)
 len(absRet.index)#325
@@ -72,6 +74,7 @@ pred_abs.to_csv (r'abstract_predictions.csv', index = False, header=True)
 
 #which PMIDs missing between abstracts retrieved list and prediction list?
 pred_abs['Year Missing']=np.where(absRet.Year != pred_abs.Year, 'True',"False")
+
 #print predictions for full text retrieved
 
 #calculate works savings from starting IDs, abstracts retreived to full text screened
