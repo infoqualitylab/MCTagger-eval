9138e3d1115a1aa28adf4debb6f4a3bc41a0651e
BrandiPSmith <brandis2@illinois.edu>
2020-04-14 21:21:00 -0500

    Update to pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -67,7 +67,7 @@ len(absRet.index)#325
 #extract predictions for abstracts retrieved
 pred_abs = pd.merge(absRet, rctAll, on=['PMID'], how='inner')
 print(pred_abs)
-len(pred_abs)#253
+len(pred_abs)#278
 
 #print predictions for abstracts retrieved
 pred_abs.to_csv (r'abstract_predictions.csv', index = False, header=True)
