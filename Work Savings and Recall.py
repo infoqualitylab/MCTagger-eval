296812ea09417fbb43e97cf1845e42b6b5321779
Rproescholdt <57776460+Rproescholdt@users.noreply.github.com>
2020-05-09 21:24:33 -0500

    Work Savings and Recall.py
    Work Savings and Recall calculations

--- Work Savings and Recall.py
+++ Work Savings and Recall.py
@@ -1,35 +1,25 @@
-# import csv
-# import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 def main():
-    abstractCalc()
-    fullTextCalc()
-    includedCalc()
-#     # abstractScreened, fullText, Included = Inputs()
-#     abs_workSavings = abstractCalc()
-#     ft_workSavings, ft_recall = fullTextCalc()
-#     # inc_recall = includedCalc()
-#     threshold, numIncluded, incNumRetained = includedCalc()
-#     outputResults()
-# #
-# def Inputs():
-#     # Input abstract screened predictions here:
-#     abstractScreened = open('Tagger Scores only  - Abstract Screened - PCSK9.txt', 'r')
-#     abstractScreenedList = abstractScreened.readlines()
-#     abstractScreened.close()
-#
-#     #Input Full-Text screened predictions here:
-#     fullText = open('Full-Text Assessed - Predictions only - PCSK9.txt', 'r')
-#     fullTextList = fullText.readlines()
-#     fullText.close()
-#
-#     #Input included article predictions here:
-#     included = open('Included RCT Scores(19).txt', 'r')
-#     includedList = included.readlines()
-#     included.close()
-#
-#     return abstractScreenedList, fullTextList, includedList
-
+    # n = [0, .0001, .001, .01, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+    n, worksavings_abstract = abstractCalc()
+    o, recall_fulltext, o2, worksavings_fulltext = fullTextCalc()
+    p, recall_included = includedCalc()
+    plt.figure(figsize=(10,6), dpi=100)
+    plt.plot(o, recall_fulltext, label = "Recall during abstract screening")
+    plt.plot(o2, worksavings_fulltext, label = "Work savings during full-text screening")
+    plt.plot(n, worksavings_abstract, label = "Work savings during abstract screening")
+    plt.plot(p, recall_included, label = "Recall during full-text screening")
+    plt.xticks([0, .0001, .001, .01, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1])
+    plt.yticks([0,.2,.4,.6,.8, 1])
+    plt.xlabel('Prediction Threshold')
+    plt.ylabel('Recall and Work Savings')
+    plt.title('RCT Work Savings and Recall', fontdict={'fontsize': 20})
+    plt.legend()
+    plt.show()
+    # plt.savefig('WorkSavingsPCSK9_RCT_regular.png', dpi = 300)
 
 def abstractCalc():
     # Input abstract screened predictions here:
@@ -47,18 +37,21 @@ def abstractCalc():
     #of predictions below the threshold:
 
     n = [0, .0001, .001, .01, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+    worksavings_abstract = []
     NumDiscarded = 0
     for threshold in n:
         for prediction in abstractScreenedList:
             if ((eval(prediction.strip()))) <= threshold:
                 NumDiscarded = NumDiscarded + 1
-        abs_worksavings = NumDiscarded / numAbstractScreened
-        print("Threshold:", threshold)
-        print("Number filtered out:", NumDiscarded)
         workSavings = NumDiscarded / numAbstractScreened
+        # print("Threshold:", threshold)
+        # print("Number filtered out:", NumDiscarded)
+        # workSavings = NumDiscarded / numAbstractScreened
+        worksavings_abstract.append(workSavings)
         NumDiscarded = 0
-        print("Work Savings:", workSavings, "\n")
+        # print("Work Savings:", workSavings, "\n")
     # return abs_worksavings
+    return n, worksavings_abstract
 
 
 def fullTextCalc():
@@ -76,35 +69,44 @@ def fullTextCalc():
     ##Recall (the number of predictions above a certain threshold, calculated
     #using full-text assessed and included):
 
-    n = [0, .0001, .001, .01, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+    o = [0, .0001, .001, .01, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+    recall_fulltext = []
     NumRetained = 0
-    for threshold in n:
+    for threshold in o:
         for prediction in fullTextList:
             if ((eval(prediction.strip()))) >= threshold:
                 NumRetained = NumRetained + 1
-        ft_recall = NumRetained / numFullText
-        print("Threshold:", threshold)
-        print("Number retained:", NumRetained)
+        # ft_recall = NumRetained / numFullText
+        # print("Threshold:", threshold)
+        # print("Number retained:", NumRetained)
         Recall = NumRetained / numFullText
+        recall_fulltext.append(Recall)
         NumRetained = 0
-        print("Percent recall:", Recall, "\n")
+        # print("Percent recall:", Recall, "\n")
+    # print(n)
+    # print(recall_full_text)
+
+
 
     ##For work savings, input files are abstract screened and full-text screened, and calculate the percent
     #of predictions below the threshold:
 
-    n = [0, .0001, .001, .01, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+# def fullTextCalc2():
+    o2 = [0, .0001, .001, .01, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+    worksavings_fulltext = []
     NumDiscarded = 0
-    for threshold in n:
+    for threshold in o2:
         for prediction in fullTextList:
             if ((eval(prediction.strip()))) <= threshold:
                 NumDiscarded = NumDiscarded + 1
         # ft_worksavings = NumDiscarded / numFullText
-        print("Threshold:", threshold)
-        print("Number filtered out:", NumDiscarded)
+        # print("Threshold:", threshold)
+        # print("Number filtered out:", NumDiscarded)
         workSavings = NumDiscarded / numFullText
+        worksavings_fulltext.append(workSavings)
         NumDiscarded = 0
-        print("Work Savings:", workSavings, "\n")
-    # return ft_recall, ft_worksavings
+        # print("Work Savings:", workSavings, "\n")
+    return o, recall_fulltext, o2, worksavings_fulltext
 
 
 def includedCalc():
@@ -122,26 +124,22 @@ def includedCalc():
     ##Recall (the number of predictions above a certain threshold, calculated
     #using full-text assessed and included):
 
-    n = [0, .0001, .001, .01, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+    p = [0, .0001, .001, .01, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+    recall_included = []
     incNumRetained = 0
-    for threshold in n:
+    for threshold in p:
         for prediction in includedList:
             if ((eval(prediction.strip()))) >= threshold:
                 incNumRetained = incNumRetained + 1
-            #     return incNumRetained
         inc_recall = incNumRetained / numIncluded
-        print("Threshold:", threshold)
-        print("Number retained:", incNumRetained)
+        # print("Threshold:", threshold)
+        # print("Number retained:", incNumRetained)
         Recall = incNumRetained / numIncluded
+        recall_included.append(Recall)
         incNumRetained = 0
-        print("Percent recall:", Recall, "\n")
-        ##COULD I WRITE OUT CSV HERE?
-        # outfile = open('inc_recall.csv', 'w')
-        # csvout = csv.writer(outfile)
-        # # row = threshold, inc_recall
-        # csvout.writerow(row)
+        # print("Percent recall:", Recall, "\n")
     # return threshold, numIncluded, incNumRetained
-
+    return p, recall_included
 # def outputResults():
 #     # threshold, numIncluded, incNumRetained = includedCalc()
 #     incRecall = incNumRetained / numIncluded
