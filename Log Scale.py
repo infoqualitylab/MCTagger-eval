74e3ce68b1db5dfb393c8457d8c32774178724cb
Rproescholdt <57776460+Rproescholdt@users.noreply.github.com>
2020-05-25 13:27:39 -0500

    Log Scale.py
    A line graph of prediction values with a logarithmic scale.

---
+++ Log Scale.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+plt.style.use('fivethirtyeight')
+
+#Get x's and y's for abstract screened:
+abstractScreened = open("Abstract Screened Predictions (148) - PCSK9.txt", 'r')
+abstractScreenedList = abstractScreened.readlines()
+abstractScreened.close()
+
+abstractScreenedList.sort(reverse = True)
+#Predictions need to be sorted; otherwise, the lines will not have visible order
+
+abs_prediction_list = [] #This will be our y
+for prediction in abstractScreenedList:
+    abs_prediction_list.append(eval(prediction.strip()))
+
+num_abstract_screened = 0
+num_abstract_list = [] #This will be our x
+
+for prediction in abstractScreenedList:
+    num_abstract_screened = num_abstract_screened + 1
+    num_abstract_list.append(num_abstract_screened)
+print("Number of articles abstract screened:", num_abstract_screened, "\n") #Just checking that the number looks right
+# print(num_abstract_list)
+
+#Get x's and y's for Full-Text Screened
+full_text = open('Full-Text Assessed - Predictions only - PCSK9.txt', 'r')
+full_text_list = full_text.readlines()
+full_text.close()
+
+full_text_list.sort(reverse = True)
+
+full_text_prediction_list = []
+for prediction in full_text_list:
+    full_text_prediction_list.append(eval(prediction.strip()))
+
+num_ft_screened = 0
+num_ft_list = []
+
+for prediction in full_text_prediction_list:
+    num_ft_screened = num_ft_screened + 1
+    num_ft_list.append(num_ft_screened)
+print("Number of articles full-text screened:", num_ft_screened, "\n")
+# print(num_ft_list)
+
+
+#Get x's and y's for included articles
+included = open('Included RCT Scores(19).txt', 'r')
+included_list = included.readlines()
+included.close()
+
+included_list.sort(reverse = True)
+
+included_prediction_list = []
+for prediction in included_list:
+    included_prediction_list.append(eval(prediction.strip()))
+
+num_included = 0
+num_included_list = []
+
+for prediction in included_prediction_list:
+    num_included = num_included + 1
+    num_included_list.append(num_included)
+print("Number of articles included in the review:", num_included, "\n")
+# print(num_included_list)
+
+plt.figure(figsize=(12, 6), dpi=100)
+
+#Plot the 3 lines
+plt.plot(num_abstract_list, abs_prediction_list, label = "Abstract Screened")
+plt.plot(num_ft_list, full_text_prediction_list, label = "Full-Text Screened")
+plt.plot(num_included_list, included_prediction_list, label = "Included")
+
+#And a benchmark line
+plt.plot([0, num_abstract_screened], [.01, .01], linewidth = 2, linestyle = "--",
+         label = ".01 (Recommended prediction value for filtering")
+
+# plt.xlim(num_abstract_screened, 0)
+# plt.ylim(1, .00001)
+
+plt.yscale('log')
+plt.yticks(labels = (".00001", ".0001", ".001", ".01", ".1", "1"), ticks = (.00001, .0001, .001, .01, .1, 1))
+# plt.title("Cumulative Frequency of Max Predictions for Long Acting Insulins Report:\nCase Control, RCTs, and Cohort Studies")
+plt.title("Cumulative Frequency of RCT Predictions for PCSK9 Report")
+plt.xlabel("Number of Predictions above Prediction Value")
+plt.ylabel("RCT Predictions")
+plt.legend(loc = 'best')
+
+plt.tight_layout(pad = 3)
+
+plt.show()
+# plt.savefig('Log Scale - PCSK9 - RCT.png', dpi = 300)
