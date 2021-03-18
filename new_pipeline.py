8645bdbed5eac608c7da18a57df1cc7cdfe82a0e
Rproescholdt <57776460+Rproescholdt@users.noreply.github.com>
2021-03-17 18:34:40 -0700

    Update and rename exp_new_pipeline.py to new_pipeline.py

--- new_pipeline.py
+++ new_pipeline.py
@@ -3,23 +3,23 @@ import csv
 
 
 cols_list = ['PMID', 'Randomized Controlled Trial']
-df = pd.read_csv("/Users/eat_p/OneDrive/Documents/RA/NIH/mt_modelscores_20210107.tsv", 'r', delimiter="\t", usecols=cols_list)
+df = pd.read_csv("/Users/randiproescholdt/Documents/Files from HP/RA/NIH/mt_modelscores_20210107.tsv", 'r', delimiter="\t", usecols=cols_list)
 prediction_data = csv.reader(df)
 
 #READ PMIDS FROM EACH STAGE OF REVIEW
 #THREE INPUT FILES GO HERE
 input_cols_list = ['PMID']
-abs_screened=pd.read_csv('/Users/eat_p/OneDrive/Documents/RA/NIH/DERP Reports/PCSK9 Inhibitors Small Streamlined Review/Input Files/Abstract Screened - PMIDs - PCSK9.csv', 'r', usecols=input_cols_list)
-ft_screened=pd.read_csv('/Users/eat_p/OneDrive/Documents/RA/NIH/DERP Reports/PCSK9 Inhibitors Small Streamlined Review/Input Files/Full-Text Screened - PMIDs - PCSK9.csv', 'r', usecols=input_cols_list)
-included=pd.read_csv('/Users/eat_p/OneDrive/Documents/RA/NIH/DERP Reports/PCSK9 Inhibitors Small Streamlined Review/Input Files/Included - PMIDs - PCSK9.csv', 'r', usecols=input_cols_list)
+abs_screened=pd.read_csv('/Users/randiproescholdt/Documents/Files from HP/RA/NIH/DERP Reports/Asthma-COPD/Abstract Screened - PMIDs.csv', 'r', usecols=input_cols_list)
+ft_screened=pd.read_csv('/Users/randiproescholdt/Documents/Files from HP/RA/NIH/DERP Reports/Asthma-COPD/Full-Text - PMIDs.csv', 'r', usecols=input_cols_list)
+included=pd.read_csv('/Users/randiproescholdt/Documents/Files from HP/RA/NIH/DERP Reports/Asthma-COPD/Included - PMIDs.csv', 'r', usecols=input_cols_list)
 
 number = 0
 pmid_list = []
 prediction_list = []
 
-#RETRIEVE PREDICTIONS FOR ABSTRACT SCREENED ITEMS (ALL ITEMS IN REVIEW WITH PMIDS)
-#NAME OUTPUT FILE APPROPRIATELY
-with open('abstract_screened_predictions_RCT_PCSK9.csv', 'w') as csvfile:
+#RETRIEVE PREDICTIONS FOR ABSTRACT SCREENED ITEMS (ALL ITEMS IN REVIEW THAT HAVE PMIDS)
+#RENAME THREE OUTPUT FILES APPROPRIATELY
+with open('abstract_screened_predictions_RCT_Asthma-COPD.csv', 'w') as csvfile:
     fieldnames = ['PMID', 'Prediction']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
@@ -45,9 +45,9 @@ print("Abstract screened items with no prediction found:", missing_row_list)
 
 #RETRIEVE PREDICTIONS FOR FULL-TEXT SCREENED ITEMS (FROM THE LIST OF ABSTRACT SCREENED PREDICTIONS)
 number = 0
-ft_missing_list = []
+# ft_missing_list = []
 
-with open('full_text_screened_predictions_RCT_PCSK9.csv', 'w') as csvfile:
+with open('full_text_screened_predictions_RCT_Asthma-COPD.csv', 'w') as csvfile:
     fieldnames = ['PMID', 'Prediction']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
@@ -56,19 +56,17 @@ with open('full_text_screened_predictions_RCT_PCSK9.csv', 'w') as csvfile:
         for row in ft_screened['PMID']:
             if item == row:
                 writer.writerow({'PMID': item, 'Prediction': prediction_list[number]})
-            else:
-                ft_missing_list.append(row)
         number = number + 1
 
-print("In full-text screened, but missing from abstract screened:", ft_missing_list)
+# print("In full-text screened, but missing from abstract screened:", ft_missing_list)
 
 
 
 #RETRIEVE PREDICTIONS FOR INCLUDED ITEMS (FROM THE LIST OF FULL-TEXT SCREENED PREDICTIONS)
 number = 0
-inc_missing_list = []
+# inc_missing_list = []
 
-with open('included_predictions_RCT_PCSK9.csv', 'w') as csvfile:
+with open('included_predictions_RCT_Asthma-COPD.csv', 'w') as csvfile:
     fieldnames = ['PMID', 'Prediction']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
@@ -77,8 +75,6 @@ with open('included_predictions_RCT_PCSK9.csv', 'w') as csvfile:
         for row in included['PMID']:
             if item == row:
                 writer.writerow({'PMID': item, 'Prediction': prediction_list[number]})
-            else:
-                inc_missing_list.append(row)
         number = number + 1
 
-print("In included, but missing from full-text screened:", inc_missing_list)
+# print("In included, but missing from full-text screened:", inc_missing_list)
